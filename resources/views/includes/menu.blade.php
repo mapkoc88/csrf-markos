e716a4a52252de7edd264ecdab47547db93ec7b6
mapkoc88 <mjc93@njit.edu>
2018-10-23 15:24:40 -0400

    Created PagesController

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -15,6 +15,12 @@
             <li class="nav-item">
                 <a class="nav-link" href="/contact">Contact</a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/register">Register</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/login">Login</a>
+            </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
                 <div class="dropdown-menu" aria-labelledby="dropdown01">
